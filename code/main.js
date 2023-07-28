c4b31dfef142dae6d81e64a38bc20a36d09fd579
Arianna Vega <ariannavega2021@gmail.com>
2023-07-27 22:25:09 -0400

    changed: html section to js and css modifidied

--- code/main.js
+++ code/main.js
@@ -1,5 +1,42 @@
+const cardsContainer = document.getElementById("cards-container");
 const projects = [
-    {
+  {
+    img: "../img/yardsale.jpg",
+    linkApp: "https://yard-sale-tan.vercel.app/",
+    title: "Yardsale",
+    description: "A simple online website for sales",
+    linkButton: "https://github.com/ariannagutirrzz/YardSale",
+    id: 1,
+  },
+  {
+    img: "../img/mokepon.png",
+    linkApp: "https://mokepon-one.vercel.app/",
+    title: "Mokepon",
+    description: "A battle game based on pokemon",
+    linkButton: "https://github.com/ariannagutirrzz/mokepon",
+    id: 2,
+  },
+  
+];
+
+function htmlWork() {
+  const recorrido = projects.map((project) => {
+    return `  
+    <div class="cards">
+        <a href=${project.linkApp}>
+            <img src=${project.img} alt="project image" class="presentation">
+        </a>
+
+        <h2>${project.title}</h2>
+        <p>${project.description}</p>
         
-    }
-]
+        <button>
+            <img src="../img/github-white.svg" alt="project image" class="button-icon">
+            <a href=${project.linkButton} target="_blank">Check this repository</a>
+        </button>
+    </div> `;
+  });
+  cardsContainer.innerHTML += recorrido.join('');
+}
+
+htmlWork();
